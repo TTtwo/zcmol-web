820df0ba99852135d8ba5105c8242cfcad3702b8
TTtwp <15917492541@163.com>
2018-06-17 23:45:16 +0800

    commit

---
+++ front-webpage/src/components/common/AboutMeComponent.vue
@@ -0,0 +1,16 @@
+<style lang="less" scoped>
+  #about-me {
+
+  }
+</style>
+
+<template>
+  <div id="about-me">
+
+  </div>
+</template>
+<script>
+  export default {
+    name: 'aboutMe'
+  }
+</script>
